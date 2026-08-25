ea93b3d4cd1870880d91c675a590c01e6e46c6ee
grimaldoenrique656-spec <grimaldoenrique656@gmail.com>
2026-08-25 13:00:11 -0600

    Add files via upload

---
+++ import tkinter as tk.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+import mysql.connector
+
+def seleccionaTabla():
+    print("has hecho click en un boton")
+
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="universidad"
+)
+cursor = conexion.cursor()
+
+ventana = tk.Tk()
+ventana.geometry("1280x720")
+ventana.title("Programa de gestión v0.1")
+
+# LISTADO DINAMICO DE LAS TABLAS DE LA BASE DE DATOS
+tablas = ttk.LabelFrame(ventana, text="Tablas", borderwidth=1, relief="ridge")
+tablas.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+cursor.execute("SHOW tables IN universidad")
+listadotablas = cursor.fetchall()
+for tabla in listadotablas:
+    tk.Button(tablas, text=tabla[0], command=seleccionaTabla).pack(padx=5, pady=5)
+
+# DATOS QUE CONTIENE LA TABLA SELECCIONADA
+datos = ttk.LabelFrame(ventana, text="Datos", borderwidth=1, relief="ridge")
+datos.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+tk.Label(datos, text="Aquí ponemos los datos").pack(padx=10, pady=10)
+
+# FORMULARIO DE DATOS DEL CAMPO SELECCIONADO
+formulario = ttk.LabelFrame(ventana, text="Formulario", borderwidth=1, relief="ridge")
+formulario.grid(row=0, column=2, sticky="nsew", padx=10, pady=10)
+tk.Label(formulario, text="Aquí ponemos el formulario").pack(padx=10, pady=10)
+
+ventana.grid_columnconfigure(0, weight=10)
+ventana.grid_columnconfigure(1, weight=80)
+ventana.grid_columnconfigure(2, weight=10)
+ventana.grid_rowconfigure(0, weight=1)
+
+ventana.mainloop()
